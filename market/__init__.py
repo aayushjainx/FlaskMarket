7f70591d8b6c903342f3ed6536733aaebcbf641b
Aayush Jain <jaayush014@gmail.com>
2021-04-30 20:21:26 +0530

    user auth 1

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '2e0b87ebe56b460c77e7ba29'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
